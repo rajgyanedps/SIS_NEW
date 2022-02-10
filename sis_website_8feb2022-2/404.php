217e207c1bfeb3f45415be77e8035fc4d0ee125f
rajgyanedps <rajendra.singh@dpsgs.org>
2022-02-10 14:50:07 +0530

    Create 404.php
    xvxvxc

---
+++ sis_website_8feb2022-2/404.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="keywords" content="">
+<meta name="robots"content="index,follow">
+<meta name="googlebot"content="INDEX,FOLLOW"/>
+<meta name="YahooSeeker"content="INDEX,FOLLOW"/>
+<meta name="msnbot" content="INDEX,FOLLOW"/>
+
+
+
+
+<title>Thank You | Selaqui International School</title>
+
+<?php include('include/class.php'); ?>
+
+<link rel="shortcut icon" href="<?=$baseurl;?>assets/images/favicon.ico" type="image/x-icon"/>
+
+
+<script language="JavaScript" type="text/javascript">
+	function refreshCaptcha()
+	{
+		var img = document.images['captchaimg'];
+		img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
+	}
+</script>
+
+
+</head>
+<body id="other_page">
+
+
+	<!-- Header Area wrapper Starts -->
+	<header id="header-wrap">
+		<!-- Navbar Start -->
+		<?php include('include/menu.php'); ?>
+		<!-- Navbar End -->
+	</header>
+	<!-- Header Area wrapper End -->
+	
+
+		
+		<section id="thankyou">
+			<div id="subscribe" class="section-padding">
+				<div class="container">
+					<div class="row justify-content-md-center">
+						<div class="col-md-10 col-lg-7">
+							<div class="subscribe-inner wow fadeInDown" data-wow-delay="0.3s">
+								<h2 class="subscribe-title">RANDOM ERROR PAGE</h2>
+								<h1 style="font-size:100px !important; ">404</h1>
+								<p class="wow fadeInDown" data-wow-delay="0.2s">Oops! Page Not Found</p>
+								<p class="wow fadeInDown" data-wow-delay="0.2s">Something Went Wrong<br>Go Back To Your Home Page Or You May Also Refresh The Page</p>
+								<a href="<?=$baseurl;?>" class="btn btn-common">Return Home</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php include('include/footer.php'); ?>	
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="<?=$baseurl;?>assets/js/jquery-min.js"></script>
+		<script src="<?=$baseurl;?>assets/js/popper.min.js"></script>
+		<script src="<?=$baseurl;?>assets/js/jquery.mCustomScrollbar.min.js"></script>
+		<script src="<?=$baseurl;?>assets/js/jquery.newsTicker.js"></script>
+		<script src="<?=$baseurl;?>assets/js/bootstrap.min.js"></script>
+		<script src="<?=$baseurl;?>assets/js/jquery.countdown.min.js"></script>
+		<script src="<?=$baseurl;?>assets/js/jquery.nav.js"></script>
+		<script src="<?=$baseurl;?>assets/js/jquery.easing.min.js"></script>
+		<script src="<?=$baseurl;?>assets/js/wow.js"></script>
+		<script src="<?=$baseurl;?>assets/js/jquery.slicknav.js"></script>
+		<script src="<?=$baseurl;?>assets/js/nivo-lightbox.js"></script>
+		<script src="<?=$baseurl;?>assets/js/main.js"></script>
+		<script src="<?=$baseurl;?>assets/js/menu_jquery.js"></script>
+    <!--<script src="<?=$baseurl;?>assets/js/form-validator.min.js"></script>
+    	<script src="<?=$baseurl;?>assets/js/contact-form-script.min.js"></script>-->
+    	<script src="<?=$baseurl;?>assets/js/form-validation.js"></script>
+    	<script>
+    		$(document).ready(function(){
+    			$(".slide-toggle").click(function(){
+    				$(".box").animate({
+    					width: "toggle"
+    				});
+    			});
+    		});
+    	</script>
+    	
+    	<script src="<?=$baseurl;?>assets/js/jquery.logoslider.js"></script>
+    	<script>
+    		$(function() {
+    			$('.js-imageslider').imageslider({
+    				slideItems: '.my-slider-item',
+    				slideContainer: '.my-slider-list',				
+    				slideDistance: 5,
+    				resizable: true,
+    				pause: true
+    			});
+    		});
+    	</script>
+    	
+    </body>
+    <script> 
+    	$(window).load(function(){
+    		$('code.language-javascript').mCustomScrollbar();
+    	});
+    	var nt_example1 = $('#nt-example1').newsTicker({
+    		row_height: 60,
+    		max_rows: 4,
+    		duration: 4000,
+    		prevButton: $('#nt-example1-prev'),
+    		nextButton: $('#nt-example1-next')
+    	});
+    </script>
+    
+    </html>
